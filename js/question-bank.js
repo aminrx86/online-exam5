2d97de0be5211b94b4b326779edff0c11f55cb91
aminrx86 <aminrx86@gmail.com>
2026-06-17 08:28:52 +0330

    Add files via upload

--- js/question-bank.js
+++ js/question-bank.js
@@ -1,16 +1,22 @@
 import { supabase } from "./supabase-client.js";
 
 export class QuestionBank {
+  constructor() {
+    this.questions = [];
+  }
+
   async loadQuestions() {
     const { data, error } = await supabase
       .from("questions")
-      .select("id, question, options");
+      .select("id, question, options")
+      .order("id"); // ❌ نه question_id
 
     if (error) {
-      console.log("Error:", error);
+      console.error(error);
       return [];
     }
 
+    this.questions = data;
     return data;
   }
 }
