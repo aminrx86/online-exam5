f5c19b9dab92539cf9fd064a848e3e8bcfb2bdcd
aminrx86 <aminrx86@gmail.com>
2026-06-17 08:52:10 +0330

    Update supabase-client.js

--- js/supabase-client.js
+++ js/supabase-client.js
@@ -1,6 +1,4 @@
-import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
+const SUPABASE_URL = "YOUR_SUPABASE_URL";
+const SUPABASE_ANON_KEY = "YOUR_SUPABASE_ANON_KEY";
 
-const SUPABASE_URL = "اینجا URL پروژه";
-const SUPABASE_ANON_KEY = "اینجا anon key";
-
-export const supabase = createClient(https://ajikhmfbufnnpubnmenp.supabase.co/rest/v1/,eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFqaWtobWZidWZubnB1Ym5tZW5wIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE1NzkyNjksImV4cCI6MjA5NzE1NTI2OX0.DahfrvuG_N881ML9hnvQie2ufokv9AhEF0DHe1qj6o4);
+window.supabase = supabase.createClient(https://ajikhmfbufnnpubnmenp.supabase.co/rest/v1/,eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFqaWtobWZidWZubnB1Ym5tZW5wIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE1NzkyNjksImV4cCI6MjA5NzE1NTI2OX0.DahfrvuG_N881ML9hnvQie2ufokv9AhEF0DHe1qj6o4);
