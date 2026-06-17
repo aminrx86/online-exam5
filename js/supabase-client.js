cc3cb71c9fc45f1f5a71efc53acbdd12cefb099f
aminrx86 <aminrx86@gmail.com>
2026-06-17 08:19:59 +0330

    Add files via upload

---
+++ js/supabase-client.js
@@ -0,0 +1,6 @@
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
+
+const SUPABASE_URL = "اینجا URL پروژه";
+const SUPABASE_ANON_KEY = "اینجا anon key";
+
+export const supabase = createClient(https://ajikhmfbufnnpubnmenp.supabase.co/rest/v1/,eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFqaWtobWZidWZubnB1Ym5tZW5wIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE1NzkyNjksImV4cCI6MjA5NzE1NTI2OX0.DahfrvuG_N881ML9hnvQie2ufokv9AhEF0DHe1qj6o4);
